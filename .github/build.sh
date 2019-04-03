617eca412e606d86f846599f7c98530699d5e5f0
Matthias Rosenstock <matthias@berla.net>
2019-04-03 08:36:11 +0200

    Strip debug information from binary

--- .github/build.sh
+++ .github/build.sh
@@ -10,7 +10,7 @@ for GOOS in darwin linux windows; do
 	SUFFIX=`[ $GOOS = "windows" ] && echo ".exe"`
 	for GOARCH in 386 amd64; do
 		TARGET=.release/$GOOS/$GOARCH/eager$SUFFIX
-		go build -v -o $TARGET
+		go build -ldflags="-s -w" -v -o $TARGET
 		tar --transform 's/.*\///g' -czvf .dist/eager-$GOOS-$GOARCH.tar.gz $TARGET README.md LICENSE
 	done
 done
